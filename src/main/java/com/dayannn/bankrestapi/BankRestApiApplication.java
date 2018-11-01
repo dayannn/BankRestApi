dc694a0334d94222537bf24c26d6976ec14ed23b
dayannn <dayannn@yandex.ru>
2018-11-02 00:12:00 +0300

    Initial commit

---
+++ src/main/java/com/dayannn/bankrestapi/BankRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.dayannn.bankrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRestApiApplication.class, args);
+	}
+}
