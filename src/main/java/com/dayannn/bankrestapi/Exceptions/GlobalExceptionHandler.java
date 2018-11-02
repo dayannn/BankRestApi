ee1b74873b513a3a0564c7114c4dc130b93e949c
dayannn <dayannn@yandex.ru>
2018-11-03 02:21:16 +0300

    Minor fixes

--- src/main/java/com/dayannn/bankrestapi/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dayannn/bankrestapi/Exceptions/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.security.auth.login.AccountNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
--- src/main/java/com/dayannn/bankrestapi/Exceptions/NotEnoughMoneyException.java
+++ src/main/java/com/dayannn/bankrestapi/Exceptions/NotEnoughMoneyException.java
@@ -2,6 +2,6 @@ package com.dayannn.bankrestapi.Exceptions;
 
 public class NotEnoughMoneyException extends RuntimeException {
     public NotEnoughMoneyException(Integer id, Long money_amount){
-        super("Bank account " + id + " doesn't has " + money_amount + " of money");
+        super("Bank account " + id + " doesn't have " + money_amount + "$");
     }
 }
